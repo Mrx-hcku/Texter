aeed9319f75492a25bf29bb1e7d858603eafed8e
Mrx-hcku <arai37562@gmail.com>
2026-08-27 08:56:28 +0530

    Fix ads schema mismatch, add targetType field

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:app_links/app_links.dart';
 import 'config/theme.dart';
 import 'services/appwrite_service.dart';
 import 'services/ads_service.dart';
 import 'screens/login_screen.dart';
 import 'screens/main_nav_screen.dart';
+import 'screens/verify_email_screen.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,12 +15,43 @@ void main() {
   runApp(const TexterApp());
 }
 
-class TexterApp extends StatelessWidget {
+class TexterApp extends StatefulWidget {
   const TexterApp({super.key});
+
+  @override
+  State<TexterApp> createState() => _TexterAppState();
+}
+
+class _TexterAppState extends State<TexterApp> {
+  final _appLinks = AppLinks();
+
+  @override
+  void initState() {
+    super.initState();
+    _listenForVerificationLinks();
+  }
+
+  void _listenForVerificationLinks() {
+    _appLinks.uriLinkStream.listen((uri) async {
+      if (uri.host == 'verify') {
+        final userId = uri.queryParameters['userId'];
+        final secret = uri.queryParameters['secret'];
+        if (userId == null || secret == null) return;
+        try {
+          await AppwriteService.instance.confirmVerification(userId: userId, secret: secret);
+          navigatorKey.currentState?.pushAndRemoveUntil(
+            MaterialPageRoute(builder: (_) => const MainNavScreen()),
+            (route) => false,
+          );
+        } catch (_) {}
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Texter',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
@@ -36,10 +71,10 @@ class _AuthGate extends StatelessWidget {
         if (snapshot.connectionState != ConnectionState.done) {
           return const Scaffold(body: Center(child: CircularProgressIndicator()));
         }
-        if (snapshot.data != null) {
-          return const MainNavScreen();
-        }
-        return const LoginScreen();
+        final user = snapshot.data;
+        if (user == null) return const LoginScreen();
+        if (!user.emailVerification) return const VerifyEmailScreen();
+        return const MainNavScreen();
       },
     );
   }
