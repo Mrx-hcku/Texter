0dfebbee273a87019fe8f7b81d04b6b59482cffb
Mrx-hcku <arai37562@gmail.com>
2026-09-16 10:22:24 +0530

    Refactor cardTheme to use CardTheme

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -146,7 +146,7 @@ class AppTheme {
           hintStyle: GoogleFonts.inter(color: dayTextSecondary, fontSize: 14),
           labelStyle: GoogleFonts.inter(color: dayTextSecondary, fontSize: 13),
         ),
-        cardTheme: CardThemeData(
+        cardTheme: CardTheme(
           color: daySurface,
           elevation: 1,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
